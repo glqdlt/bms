b3925f13c484558bd4a198a3e4a6dcad02afbf33
glqdlt <dlfdnd0725@gmail.com>
2018-03-31 17:54:21 +0900

    improve : app controller

--- bms-server/src/main/java/com/glqdlt/bmscommon/bmsserver/controller/UserRestController.java
+++ bms-server/src/main/java/com/glqdlt/bmscommon/bmsserver/controller/UserRestController.java
@@ -1,17 +1,19 @@
 package com.glqdlt.bmscommon.bmsserver.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 /**
  * Created By iw.jhun
  * On 2018-03-30 , 오후 3:07
  */
 
+@RequestMapping("/user")
 @RestController
-public class SimpleRestController {
+public class UserRestController {
 
-    @RequestMapping("/")
-    public void home(){
+    @RequestMapping(value = "/join",method = RequestMethod.POST)
+    public void createUser(){
 
     }
 
