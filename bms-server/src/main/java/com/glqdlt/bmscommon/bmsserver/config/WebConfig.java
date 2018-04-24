706c7039c6adc22d7bee01097dc71fb2ac4a92c1
dlfdnd0725@gmail.com <dlfdnd0725@gmail.com>
2018-04-24 17:14:36 +0900

    new : 인증 Interceptor 뼈대

--- bms-server/src/main/java/com/glqdlt/bmscommon/bmsserver/config/WebConfig.java
+++ bms-server/src/main/java/com/glqdlt/bmscommon/bmsserver/config/WebConfig.java
@@ -1,9 +1,20 @@
 package com.glqdlt.bmscommon.bmsserver.config;
 
+import com.glqdlt.bmscommon.bmsserver.interceptor.CertificationInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-//@Configuration
+@Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+//        super.addInterceptors(registry);
+        registry
+                .addInterceptor(new CertificationInterceptor())
+                .addPathPatterns("/*")
+                .excludePathPatterns("/login");
 
+    }
 }
