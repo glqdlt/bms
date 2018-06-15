8bbd8e2a344c104b11b96b7c1d4996bc75bc6c5d
iw.jhun <dlfdnd0725@gmail.com>
2018-06-15 23:57:41 +0900

    wakeonlan 기능에서 serverId로 server 정보를 가져오는 로직 추가

--- bms-server/src/main/java/com/glqdlt/bmsserver/wol/service/WakeOnLanService.java
+++ bms-server/src/main/java/com/glqdlt/bmsserver/wol/service/WakeOnLanService.java
@@ -4,8 +4,8 @@ import com.glqdlt.bmscommon.persistence.server.entity.Server;
 
 public interface WakeOnLanService {
 
-     void powerOn(Server server);
-     void shutdown(Server server);
-     void reStart(Server server);
+     void powerOn(Integer serverId);
+     void shutdown(Integer serverId);
+     void reStart(Integer serverId);
 
 }
--- bms-server/src/main/java/com/glqdlt/bmsserver/wol/service/WakeOnLanServiceImpl.java
+++ bms-server/src/main/java/com/glqdlt/bmsserver/wol/service/WakeOnLanServiceImpl.java
@@ -1,6 +1,7 @@
 package com.glqdlt.bmsserver.wol.service;
 
 import com.glqdlt.bmscommon.persistence.server.entity.Server;
+import com.glqdlt.bmscommon.persistence.server.repo.ServerRepo;
 import com.glqdlt.bmsserver.wol.WakeOnLan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,21 +9,29 @@ import org.springframework.stereotype.Service;
 @Service
 public class WakeOnLanServiceImpl implements WakeOnLanService {
 
+
+    private static final Integer BROAD_CAST_PORT = 9;
+
     @Autowired
     WakeOnLan wakeOnLan;
 
+    @Autowired
+    ServerRepo serverRepo;
+
     @Override
-    public void powerOn(Server server) {
-        wakeOnLan.wake(server.getMac(),server.getPort(),server.getIp());
+    public void powerOn(Integer serverId) {
+
+        Server server = serverRepo.findOne(serverId);
+        wakeOnLan.wake(server.getMac(), BROAD_CAST_PORT, server.getIp());
     }
 
     @Override
-    public void shutdown(Server server) {
+    public void shutdown(Integer serverId) {
 
     }
 
     @Override
-    public void reStart(Server server) {
+    public void reStart(Integer serverId) {
 
     }
 }
